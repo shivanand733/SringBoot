5b875e39ab2dca764c43197a9922ffe9470c1a9e
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-12 09:21:44 +0530

    create a web application

--- DemoSpring/src/main/java/com/Mr_Shivanand/DemoSpring/App.java
+++ DemoSpring/src/main/java/com/Mr_Shivanand/DemoSpring/App.java
@@ -3,7 +3,7 @@ package com.Mr_Shivanand.DemoSpring;
 public class App {
     public static void main(String[] args) {
     	Dev obj = new Dev();
-    	obj.build();
+    	obj.build(); 
     			
     }
 }

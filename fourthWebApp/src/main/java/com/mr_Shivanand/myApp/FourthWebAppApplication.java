de1b608435c73c0656cd06567692e86f8fe343fd
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-23 13:34:09 +0530

    create a Fourth Spring Projects

---
+++ fourthWebApp/src/main/java/com/mr_Shivanand/myApp/FourthWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.mr_Shivanand.myApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class FourthWebAppApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ac =  SpringApplication.run(FourthWebAppApplication.class, args);
+	}
+
+}
